c6be9d546aea3d3b2d4d1d7c922eae14f13b84ef
sGtGR4mvHXhfznmzqcHp2UFN <62153326+Kash-001@users.noreply.github.com>
2024-12-17 23:32:28 +0100

    Updated gooning.lua
    fixed alot of things :
    - added wait for data
    - fixed anti fall in void
    - fixed mine reset
    - improved selling speed
    - improved mining speed
    - improved overhaul perfs using listeners
    - cleaned some useless lines

--- gooning.lua
+++ gooning.lua
@@ -5,11 +5,15 @@ local Window = Library.CreateLib("Gooning", "BloodTheme")
 -- local Window = Library.CreateLib("Gooning", "Synapse")
 
 -- GLOBAL VARIABLES
-_G.SellingTreshold = 0
+if not _G.SellingTreshold then
+    _G.SellingTreshold = 0
+end
 
 -- GAME LOCALS
 local LocalPlayer = game.Players.LocalPlayer
 local Remote = game.ReplicatedStorage.Network:InvokeServer()
+local RunService = game:GetService("RunService")
+local RunServiceStepped = game:GetService("RunService").Stepped
 local Character = LocalPlayer.Character
 local HumanoidRootPart = Character:FindFirstChild("HumanoidRootPart")
 local InventoryAmount = LocalPlayer.PlayerGui.ScreenGui.StatsFrame2.Inventory.Amount
@@ -17,46 +21,23 @@ local Humanoid = Character:WaitForChild("Humanoid")
 local Blocks = game.Workspace.Blocks
 local GC = getconnections or get_signal_cons
 local cloneref = cloneref or function(o) return o end
+local RebirthsAmount = LocalPlayer.leaderstats.Rebirths
+local DepthAmount = LocalPlayer.PlayerGui.ScreenGui.TopInfoFrame.Depth
 
--- POSITIONS LOCALS
-local LavaMineTopPosition = Vector3.new(33, 12, 26205)
-local SellPointPosition = Vector3.new(-117.57, 10.39, 42.77)
-local LavaTeleportPadPosition = Vector3.new(-29.3160343170166, 11.087592124938965, 84.10152435302734)
-
--- TO AVOID HITTING THEM
-if game.workspace:FindFirstChild("TopMiners") then
-    game.workspace.TopMiners:Destroy()
-end
-if game.workspace:FindFirstChild("MostRebirth") then
-    game.workspace.MostRebirth:Destroy()
-end
-if game.workspace:FindFirstChild("DailyBoard") then
-    game.workspace.DailyBoard:Destroy()
-end
-
------------------------------------
------------ LISTENERS -------------
------------------------------------
-game.Workspace.Collapsed.Changed:connect(function()
-    if IsAutoFarmEnabled then
-	wait(1)
-        Humanoid:MoveTo(LavaMineTopPosition)
-    end
-end)
-
-Humanoid.Died:connect(function()
-    if IsAutoFarmEnabled then
-        wait(6)
-        local NewHumanoid = game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
-        NewHumanoid:MoveTo(LavaTeleportPadPosition)
-        wait(6)
-        NewHumanoid:MoveTo(LavaMineTopPosition)
-    end
-end)
+-- POSITIONS
+local SellPointPosition = CFrame.new(41.96064, 15.8550873, -1239.64648, 1, 0, 0, 0, 1, 0, 0, 0, 1)
 
 -----------------------------------
 ----------- FUNCTIONS -------------
 -----------------------------------
+local function Split(s, delimiter)
+    result = {};
+    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+        table.insert(result, match);
+    end
+    return result;
+end
+
 local function GetInventoryAmount()
 	local Amount = InventoryAmount.Text
 	Amount = Amount:gsub('%s+', '')
@@ -83,9 +64,9 @@ local function SellInventory()
     local ActualInventorySize = GetInventoryAmount()
 
     while ActualInventorySize ~= 0 do
-        HumanoidRootPart.CFrame = CFrame.new(SellPointPosition)
-        Remote:FireServer("SellItems",{{               }})
-        wait()
+        HumanoidRootPart.CFrame = SellPointPosition
+        Remote:FireServer("SellItems",{{}})
+        RunServiceStepped:Wait()
         ActualInventorySize = GetInventoryAmount()
     end
 end
@@ -126,6 +107,40 @@ local function SendInformation(GivenInfo)
     })
 end
 
+local function GoBackMining()
+    if IsAutoFarmEnabled then
+        IsAutoFarmEnabled = false
+        local WasAutoFarmEnabled = true
+    end
+
+    Humanoid.WalkSpeed = 0
+    Humanoid.JumpPower = 0
+    HumanoidRootPart.Anchored = true
+    Remote:FireServer("MoveTo", {{"LavaSpawn"}})
+    local className = "Part"
+    local parent = game.Workspace
+    local part = Instance.new(className, parent)
+    part.Anchored = true
+    part.Size = Vector3.new(10, 0.5 , 100)
+    part.Material = "ForceField"
+    local pos = Vector3.new(21, 9.5, 26285)
+    part.Position = pos
+    wait(1)
+    HumanoidRootPart.Anchored = false
+    while HumanoidRootPart.Position.Z > 26220 do
+        HumanoidRootPart.CFrame = CFrame.new(Vector3.new(HumanoidRootPart.Position.X,13.05,HumanoidRootPart.Position.Z-0.5))
+        wait()
+    end
+    HumanoidRootPart.CFrame = CFrame.new(33, 12, 26220)
+    Humanoid.WalkSpeed = 16
+    Humanoid.JumpPower = 50
+    
+    if WasAutoFarmEnabled then
+        wait(1)
+        IsAutoFarmEnabled = true
+    end
+end
+
 -----------------------------------
 ----------- AUTO TAB --------------
 -----------------------------------
@@ -162,13 +177,6 @@ end)
 AutosTabSection:NewToggle("AutoRebirth","Autorebirth when enough money", function(state)
     if state then
         IsAutoRebirthEnabled = true
-
-        while IsAutoRebirthEnabled do
-            if GetTotalCoins() >= GetRebirthPrice() then
-                Remote:FireServer("Rebirth",{{					                }})
-            end
-            wait(0.2)
-        end
     else
         IsAutoRebirthEnabled = false
     end
@@ -189,11 +197,11 @@ AutosTabSection:NewToggle("AutoMine","Auto Mine blocks in 2 radius", function(st
                     if block:IsA("BasePart") then
                         local BlockModel = block.Parent
                         Remote:FireServer("MineBlock",{{BlockModel}})
-                        wait()
+                        RunServiceStepped:Wait()
                     end
                 end
             end
-            wait()
+            RunServiceStepped:Wait()
         end
     else
         IsAutoMineEnabled = false
@@ -204,7 +212,7 @@ AutosTabSection:NewToggle("AutoFarm","Auto Mine / Rebirth / Sell", function(stat
     if state then
         if _G.SellingTreshold > 0 then
             IsAutoFarmEnabled = true
-            Humanoid:MoveTo(LavaMineTopPosition)
+            GoBackMining()
 
             while IsAutoFarmEnabled do
                 if HumanoidRootPart then
@@ -217,17 +225,17 @@ AutosTabSection:NewToggle("AutoFarm","Auto Mine / Rebirth / Sell", function(stat
                         if block:IsA("BasePart") then
                             local BlockModel = block.Parent
                             Remote:FireServer("MineBlock",{{BlockModel}})
-                            wait()
+                            RunServiceStepped:Wait()
                         end
                         InventoryContains = GetInventoryAmount()
                         if InventoryContains >= _G.SellingTreshold then
                             SellInventory()
                             wait(0.2)
-                            Remote:FireServer("Rebirth",{{					                }})
+                            Remote:FireServer("Rebirth",{{}})
                         end
                     end
                 end
-                wait()
+                RunServiceStepped:Wait()
             end
         else
             SendError("Configure sell treshold !")
@@ -303,6 +311,12 @@ MiscTabSection:NewButton("Destroy Gooning", "Kills the UI and the scripts", func
     game.CoreGui[KavoInstanceToDestroy]:Destroy()
 end)
 
+MiscTabSection:NewButton("Halloween Shop", "Opens Halloween's shop", function()
+    HumanoidRootPart.CFrame = game.workspace.Activation.Halloween2019.CFrame
+end)
+
+
+
 -----------------------------------
 ---------- CONFIG TAB -------------
 -----------------------------------
@@ -315,4 +329,27 @@ ConfigTabSection:NewTextBox("Selling Treshold", "Enter at how much blocks you se
 	_G.SellingTreshold = tonumber(txt)
     SendInformation("Treshold set to ".._G.SellingTreshold)
     SellingTresholdLabel:UpdateLabel("Selling Treshold : ".._G.SellingTreshold)
+end)
+
+-----------------------------------
+----------- LISTENERS -------------
+-----------------------------------
+game.Workspace.Collapsed.Changed:connect(function()
+    if IsAutoFarmEnabled then
+	    wait(1)
+        GoBackMining()
+    end
+end)
+
+DepthAmount.Changed:connect(function()
+    local depth = Split(DepthAmount.Text," ")
+    if tonumber(depth[1]) >= 1000 then
+        GoBackMining()
+    end
+end)
+
+RunService:BindToRenderStep("Rebirth", Enum.RenderPriority.Camera.Value, function()
+    if (IsAutoRebirthEnabled or IsAutoFarmEnabled) and GetCoinsAmount() >= (10000000 * (RebirthsAmount.Value + 1)) then
+        Remote:FireServer("Rebirth", {{}})
+    end
 end)
